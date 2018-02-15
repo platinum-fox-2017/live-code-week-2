219cfedb6f25347e94173ef135955ea9a7ed1463
root <irsyadpahlapi28@gmail.com>
2018-02-14 23:35:52 -0500

    finishing oop and logic

--- oop-hard/index.js
+++ oop-hard/index.js
@@ -1,3 +1,4 @@
+const fs=require('fs')
 var Journal = require('./journal.js')
 var History = require('./history.js')
 var Biography = require('./biography.js')
@@ -8,20 +9,13 @@ var Readers = require('./pembaca.js')
 class Perpustakaan{
   constructor(){
     this._name='Perpustakaan Javascript'
-    this._book=this.bukuawal()
-    this._pembaca=this.addpembaca() || []
-  }
-
-  bukuawal(){
-    var books=[]
-    books.push(new Journal('Jatuh Bangung Seorang Fullstack','Kang Udin',89,1,true))
-    books.push(new Biography('Orang Dibalik Apple','Mas Bejo',327,4,true,'Steve Wozniak'))
-    books.push(new History('Awal Peradaban Callback','Bang Togar',127,2,true,'Middle Earth'))
-    return books
+    this._book=[]
+    this._pembaca=[]
   }
 
   addpembaca(nama,alamat,notelp){
-    this.pembaca.push(new Readers(nama,alamat,notelp))
+    this._pembaca.push(new Readers(nama,alamat,notelp))
+    return this
   }
 
   borrow(judulbuku,namapembaca){
@@ -41,10 +35,37 @@ class Perpustakaan{
 }
 
 
-
-
 var perpustakaan = new Perpustakaan()
+var journal = new Journal('Jatuh Bangung Seorang Fullstack','Kang Udin',89,1,true)
+perpustakaan._book.push(journal)
+var biography = new Biography('Orang Dibalik Apple','Mas Bejo',327,4,true,'Steve Wozniak')
+perpustakaan._book.push(biography)
+var history = new History('Awal Peradaban Callback','Bang Togar',127,2,true,'Middle Earth')
+perpustakaan._book.push(history)
+
+if(biography._totalpage>200){
+  console.log('Banyak halamannya capek');
+}else{
+  console.log(biography._totalpage);
+}
 console.log(perpustakaan._book);
+console.log('============== tambah pembaca==========');
+perpustakaan.addpembaca('irsyadpahlapi','jl.kejaksaan','082383222152')
+perpustakaan.addpembaca('eki','jl.bandung','08222222222')
+console.log(perpustakaan._pembaca);
+console.log();
+console.log('============== peminjaman buku ===========');
 perpustakaan.borrow('Jatuh Bangung Seorang Fullstack','irsyadpahlapi')
+console.log();
+console.log('============== cek buku ===========');
 console.log(perpustakaan._book);
+console.log();
+console.log('============== cek buku yang sudah dipinjam ===========');
 perpustakaan.borrow('Jatuh Bangung Seorang Fullstack','eki')
+
+console.log();
+console.log('============= input json =============');
+let parsingjson = JSON.stringify(perpustakaan._book)
+let parsingpembaca = JSON.stringify(perpustakaan._pembaca)
+fs.writeFile('./data.json',parsingjson,function(err){})
+fs.writeFile('./datapembaca.json',parsingpembaca,function(err){})
