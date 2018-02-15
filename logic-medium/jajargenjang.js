dfdfa7a275f96d3a210ba60e85ac2838401a0f2b
agrha <agrhakartz@gmail.com>
2018-02-15 10:14:41 +0700

    jajargenjang finished

--- logic-medium/jajargenjang.js
+++ logic-medium/jajargenjang.js
@@ -1,20 +1,32 @@
-function jajarGenjang(panjang,lebar){
+function jajarGenjang(lebar,panjang){
   let count = 0
-  let count2 = 0
+  let count2 = lebar
   let arr = []
-  for (var i = panjang; i > 0; i--) {
+  for (var i = 0; i < panjang; i++) {
     let temp = []
-    for (var j = 0; j < lebar+panjang-i; j++) {
-      if(j = lebar+panjang-i-1){
-        temp.push('|')
+    for (var j = 0; j < lebar+panjang; j++) {
+      if(i === 0 || i === panjang-1){
+        if(j >= count && j <= count2){
+          temp.push('-')
+        } else {
+          temp.push(' ')
+          }
       } else {
-        temp.push('x')
+        if(j === count || j === count2){
+          temp.push('|')
+        } else {
+          temp.push(' ')
+        }
       }
     }
-    arr.push(temp)
+    arr.push(temp.join(''))
+    count += 1
+    count2 += 1
   }
-  return arr
+  return arr.join('\n')
 }
 
 
-console.log(jajarGenjang(5,4));
+console.log(jajarGenjang(10,12));
+console.log(jajarGenjang(9,11));
+console.log(jajarGenjang(20,15));
