5f6c45fcd1c9913e0327c41bcee2c8d529b89d85
agungcaproex <agung.caproex@gmail.com>
2018-02-15 11:19:41 +0700

    sisa 25 menit

--- logic-medium/jajargenjang.js
+++ logic-medium/jajargenjang.js
@@ -1,14 +1,33 @@
 function JajarGenjang(horizontal, vertical){
+    let count = 0
+    let hor = horizontal
     let result = []
-    for(let i=0; i<horizontal; i++){
-        result[i] += '-'
-        result.push(result[i])
-        for(let j=0; j<vertical; j++){
-            result[j] = '/'
-            result.push(result[j])
+    for(let i=0; i<vertical; i++){
+        let res_temp = []
+        
+        for (let j = 0; j < vertical+horizontal; j++) {
+            if(i === 0 || i === vertical-1){
+                if(j >= count && j <= hor){
+                    res_res_temp.push('-')
+                } 
+                else {
+                    res_res_temp.push(' ')
+                }
+            }
+            else{
+                if(j === count || j === count2){
+                    res_temp.push('|')
+                  } else {
+                    res_temp.push(' ')
+                  }
+                }
+              }
+              
+          
+            }
         }
-    }
-    return result.join('')
+    }   
+    return result
 }
 
 console.log(JajarGenjang(5,4))
