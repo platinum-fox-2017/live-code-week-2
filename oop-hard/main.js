a698ec5e14c80f0adc2ac5a0d667e5ba1244a1a5
Ardhiansyah Putra <ardhimetanaputra@gmail.com>
2018-02-15 11:44:51 +0700

    Release 2 : oop-hard (with improvement)

--- oop-hard/main.js
+++ oop-hard/main.js
@@ -1,3 +1,11 @@
+/*
+NOTE:
+	* Buku yang sudah ditambah tidak bisa ditambah lagi
+	* Pembaca yang sudah terdaftar tidak bisa di daftarkan lagi
+	* Jika ingin meminjam buku harus terdaftar
+	* Buku boleh dikembalikan oleh semua pembaca yang sudah terdaftar
+*/
+
 const Journal = require('./Journal.js');
 const Biography = require('./Biography.js');
 const History = require('./History.js');
@@ -8,32 +16,36 @@ const bookPath = './book_data.json';
 const readerPath = './readers_data.json';
 
 
+/* INSTANCE LIBRARY*/
+let perpustakaan = new Library('Perpustakaan Javascript', 'Pondok Indah');
+
+
+/* INSTANCE JOURNAL, BIOGRAPHY, HISTORY*/
 let journal = new Journal('Jatuh Bangung Seorang Fullstack', 'Kang Udin', 89, 1);
 let biography = new Biography('Orang Dibalik Apple', 'Mas Bejo', 327, 4, 'Steve Wozniak');
 let history = new History('Awal Peradaban Callback', 'Bang Togar', 127, 2, 'Middle Earth');
 
-let book = [];
-book.push(journal, biography, history);
 
-let perpustakaan = new Library('Perpustakaan Javascript', 'Pondok Indah', book);
+/* INSTANCE READERS*/
+let putra = new Readers('Putra', 'Bekasi', '123456789');
+let hari = new Readers('Hari', 'Jakarta', '123456789');
+
+/* ADD BUKU */
+perpustakaan.addBook(journal);
+perpustakaan.addBook(biography);
+// perpustakaan.addBook(history);
+
+/* ADD READER */
+perpustakaan.addReader(putra);
+// perpustakaan.addReader(hari);
+
+
+// perpustakaan.borrow('Awal Peradaban Callback', 'Hari');
+perpustakaan.borrow('Orang Dibalik Apple', 'Putra');
+// perpustakaan.return('Awal Peradaban Callback', 'aa');
+// perpustakaan.return('Awal Peradaban Callback', 'Hari');
 
 console.log(biography.totalPages);
 console.log(journal.totalPages);
-// console.log(perpustakaan.book);
-
-let putra = new Readers('Putra', 'Bekasi', '123456789');
-let reader = [];
-reader.push(putra);
-
-perpustakaan.addReader(putra);
-perpustakaan.borrow('Awal Peradaban Callback', putra.name);
-console.log(perpustakaan.book);
-perpustakaan.borrow('Awal Peradaban Callback', putra.name);
-
-Library.writeFile(bookPath, book, function() {
-	console.log('Data Buku Telah Disimpan');
-});
-
-Library.writeFile(readerPath, reader, function() {
-	console.log('Data Pembaca Telah Disimpan');
-});
+perpustakaan.book;
+perpustakaan.reader;
