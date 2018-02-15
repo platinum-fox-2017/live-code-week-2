381416cbf1ec3ae884417260350deb2724e76ba0
unknown <fatimah.azzhr@gmail.com>
2018-02-15 11:19:31 +0700

    oop-hard.js

--- oop-hard/oop-hard.js
+++ oop-hard/oop-hard.js
@@ -20,6 +20,21 @@ class Library {
     }
     return arrBooks;
   }
+
+  isAvail() {
+
+  }
+
+  borrow(title, reader) {
+    let listOfBooks = this.generate_books()
+    for(let i = 0; i < listOfBooks; i++) {
+      if(title === listOfBooks[i].title) {
+        listOfBooks[i]._isAvail = false
+        listOfBooks[i]._borrower = reader
+      }
+    }
+    console.log(listOfBooks)
+  }
 }
 
 class Books {
@@ -30,7 +45,7 @@ class Books {
     this._totalPages = objBooks.totalPages
     this._readingDays = objBooks.readingDays
     this._isAvail = objBooks.isAvail
-    this._borrower = 'name'
+    this._borrower
   }
 
   get totalPages() {
@@ -39,6 +54,17 @@ class Books {
     } else {
       return this._totalPages
     }
+  }
+
+  borrow(title, reader) {
+    let listOfBooks = lib.generate_books()
+    for(let i = 0; i < listOfBooks; i++) {
+      if(title === listOfBooks[i].title) {
+        listOfBooks[i]._isAvail = false
+        listOfBooks[i]._borrower = reader
+      }
+    }
+    console.log(listOfBooks)
   }
 }
 
@@ -63,26 +89,36 @@ class History extends Books {
 }
 
 let lib = new Library()
-let books = [ {title: 'Jatuh Bangun Seorang Fullstack', genre: 'Journal',author: 'Kang Udin', totalPages: 89, readingDays: 1, isAvail: true}, 
-              {title: 'Orang Dibalik Apple', genre: 'Biography', author: 'Mas Bejo', totalPages: 327, readingDays: 4, isAvail: true, figure: 'Steve Wozniak'},
-              {title: 'Awal Peradaban Callback', genre: 'History', author: 'Bang Togar', totalPages: 127, readingDays: 2, isAvail: true, century: 'Middle Earth'}]
-console.log(lib.generate_books())
+let books = new Books({title: 'Jatuh Bangun Seorang Fullstack', genre: 'Journal',author: 'Kang Udin', totalPages: 89, readingDays: 1, isAvail: true})
+// let books = [ {title: 'Jatuh Bangun Seorang Fullstack', genre: 'Journal',author: 'Kang Udin', totalPages: 89, readingDays: 1, isAvail: true}, 
+//               {title: 'Orang Dibalik Apple', genre: 'Biography', author: 'Mas Bejo', totalPages: 327, readingDays: 4, isAvail: true, figure: 'Steve Wozniak'},
+//               {title: 'Awal Peradaban Callback', genre: 'History', author: 'Bang Togar', totalPages: 127, readingDays: 2, isAvail: true, century: 'Middle Earth'}]
+// console.log(lib.generate_books())
 
 let journal = new Journal({title: 'Jatuh Bangun Seorang Fullstack', genre: 'Journal',author: 'Kang Udin', totalPages: 89, readingDays: 1, isAvail: true});
-console.log(journal.totalPages)
+// console.log(journal.totalPages)
+// console.log(journal.borrow())
 
 let biography = new Biography({title: 'Orang Dibalik Apple', genre: 'Biography', author: 'Mas Bejo', totalPages: 327, readingDays: 4, isAvail: true, figure: 'Steve Wozniak'});
-console.log(biography.totalPages)
+// console.log(biography.totalPages)
 
 let history = new History({title: 'Awal Peradaban Callback', genre: 'History', author: 'Bang Togar', totalPages: 127, readingDays: 2, isAvail: true, century: 'Middle Earth'});
-console.log(history.totalPages)
+// console.log(history.totalPages)
 
 //------------------------------------------------------------------
 
 class Readers {
-  constructor(name, address, phone) {
-    this._name =
-    this._address
-    this._phone
+  constructor(objReaders) {
+    this._name = objReaders.name
+    this._address = objReaders.address
+    this._phone = objReaders.phone
+    this._borrowedBooks = objReaders.books
   }
 }
+
+let member = {name: 'Acim',address: 'Jakarta',phone: '87414681', books: 'Orang Dibalik Apple'}
+let reader = new Readers(member)
+// console.log(reader)
+
+lib.generate_books()
+books.borrow('Orang Dibalik Apple', 'Acim')
