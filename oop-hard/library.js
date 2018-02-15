c0a410066231f434ae4cce8ba5f3c58155424940
agungcaproex <agung.caproex@gmail.com>
2018-02-15 10:14:55 +0700

    oop release 0

--- oop-hard/library.js
+++ oop-hard/library.js
@@ -1,9 +1,26 @@
+class Library{
+    constructor(){
+
+    }
+}
+
 class Book{
     constructor(title, author, page, readTime){
         this._title = title
         this._author = author
         this._page = page
         this._readTime= this.readTime()
+    }
+
+    checkPage(){
+        let result = ''
+        if(this._page > 200){
+            result += 'Banyak halamannya, capek ngitungnya'
+        }
+        else if(this._page < 200){
+            result = this._page
+        }
+        return result
     }
 
     readTime(){
@@ -25,6 +42,7 @@ class Biography extends Book{
     constructor(title, author, page, readTime, figure){
         super(title, author, page, readTime)
         this._figure = figure
+        this._page = this.checkPage()
     }
 
 }
@@ -33,6 +51,7 @@ class History extends Book{
     constructor(title, author, page, readTime, century){
         super(title, author, page, readTime)
         this._century = century
+        this._page = this.checkPage()
     }
 
 }
@@ -44,11 +63,13 @@ let book = [
 ]
 console.log(book)
 
-// let jurnal = new Journal('Jatuh Bangun Seorang Fullstack','Kang Udin',89,null)
-// console.log(jurnal)
+let bio = new Biography('Orang Dibalik Apple','Mas Bejo',327,null,'Steve Wozniak')
+console.log(bio._page)
 
-// let bio = new Biography('Orang Dibalik Apple','Mas Bejo',327,null,'Steve Wozniak')
-// console.log(bio)
+let jurnal = new Journal('Jatuh Bangun Seorang Fullstack','Kang Udin',89,null)
+console.log(jurnal._page)
+
+
 
 // let history = new History('Awal Peradaban Callback','Bang Tagor',127,null,'Middle Earth')
 // console.log(history)
