d7b09e9336c0b1a99238a23a74d45be58494a37a
Reynaldi Ananda Pane <reynaldipane@gmail.com>
2018-02-15 11:49:02 +0700

    Release 3

--- oop-hard/driver.js
+++ oop-hard/driver.js
@@ -3,32 +3,64 @@ const Journal   = require('./Journal');
 const Bioghrapy = require('./Biography');
 const History   = require('./History');
 const Readers   = require('./Readers');
+const fs        = require('fs');
 
-let journal     = new Journal('Jatuh bangun seorang fullstack', 'Kang Udin', 89, 1, true,'');
-let bioghrapy   = new Bioghrapy('Orang dibalik Apple', 'Mas Bejo', 327, 4, true, 'Steve Wozniack','');
-let history     = new History('Awal Peradaban Callback', 'Bang Togar', 127, 2, true, 'Middle Earth','');
+
+let library     = new Library('Pondok Indah');
+
+let bookData    = fs.readFileSync('./book.JSON','utf8');
+let bookDataJSON    = JSON.parse(bookData); 
+
+for(let i = 0; i < bookDataJSON.length; i++) {
+    if(bookDataJSON[i].type == 'Journal') {
+        var journal     = new Journal(bookDataJSON[i].title, bookDataJSON[i].author, bookDataJSON[i].totalPage, bookDataJSON[i].readingDays, bookDataJSON[i].isAvail,bookDataJSON[i].borrowers);
+    } else if(bookDataJSON[i].type == 'Bioghrapy') {
+        var bioghrapy     = new Bioghrapy(bookDataJSON[i].title, bookDataJSON[i].author, bookDataJSON[i].totalPage, bookDataJSON[i].readingDays, bookDataJSON[i].isAvail,bookDataJSON[i].figure,bookDataJSON[i].borrowers);
+    } else {
+       var history        = new History(bookDataJSON[i].title, bookDataJSON[i].author, bookDataJSON[i].totalPage, bookDataJSON[i].readingDays, bookDataJSON[i].isAvail,bookDataJSON[i].century,bookDataJSON[i].borrowers)
+    }
+}
 
 console.log(bioghrapy.totalPage);
 console.log(journal.totalPage);
+console.log(" ");
 
-let library     = new Library('Pondok Indah');
+var newJournal = new Journal('my journal', 'milo', '123', '5', true);
+
+bookDataJSON.push(newJournal);
+fs.writeFileSync('./book.JSON',JSON.stringify(bookDataJSON));
 
 library.addBook(journal);
 library.addBook(bioghrapy);
 library.addBook(history);
+library.addBook(newJournal);
 
 console.log(library.book);
+console.log(" ");
 
-let readers     = new Readers("Reynaldi","Medan","081372211");
-let readers2    = new Readers("Irsan", "Pekanbaru","0811234");
-library.addReader(readers);
-library.addReader(readers2);
+let readerData          = fs.readFileSync('./reader.JSON','utf8');
+let readerDataJSON      = JSON.parse(readerData); 
+
+for(let i = 0; i < readerDataJSON.length; i++) {
+    var reader = new Readers(readerDataJSON[i].name,readerDataJSON[i].address,readerDataJSON[i].notelp);
+    library.addReader(reader);
+}
 
 console.log(library.readers);
+console.log(" ");
+
+var newReaders = new Readers('Milo', 'sidoarjo', '081133421');
+library.addReader(newReaders);
+readerDataJSON.push(newReaders);
+fs.writeFileSync('./reader.JSON',JSON.stringify(readerDataJSON));
 
 library.borrow('Orang dibalik Apple', 'Reynaldi');
-
+console.log(" ");
 console.log(bioghrapy);
+console.log(" ");
 
 library.borrow('Orang dibalik Apple', 'Irsan');
+console.log(" ");
 library.borrow('Orang dibalik Apple', 'Abdul');
+console.log(" ");
+library.borrow('my journal','Milo')
