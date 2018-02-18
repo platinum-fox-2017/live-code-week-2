881a6ec518e71a8f2e4c76fb15de0fa7dd44e1e0
Fitrul Islam <fitrul.islam@gmail.com>
2018-02-19 06:57:00 +0700

    done

--- logic-medium/logic.js
+++ logic-medium/logic.js
@@ -1,10 +1,32 @@
 function jajarGenjang(num1, num2){
+  let garisHorizontal = '';
+  let garisVertikal = '|'
+  for(let i=0; i<num1; i++){
+    if(i===num1-1){
+      garisHorizontal += '- ';
+      garisVertikal += ' |';
+    } else {
+      garisHorizontal += '- ';
+      garisVertikal += '  ';
+    }
+  }
+
+  let spasi = '';
   for(let i=0; i<=num2+1; i++){
-    let array = [];
     if(i===0){
-      for(let k=0; k<num1){
-        
-      }
+      console.log(garisHorizontal);
+    }
+    else if(i===num2+1){
+      let tambah = spasi + garisHorizontal;
+      console.log(tambah);
+    } else {
+      let tambah = spasi + garisVertikal;
+      console.log(tambah);
+      spasi += '  ';
     }
   }
 }
+
+jajarGenjang(5,4);
+jajarGenjang(3,3);
+jajarGenjang(2,4);
